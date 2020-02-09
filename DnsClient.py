4fe82de2ebf3f1b20bda649e91cfe6ba8a8385d7
Pengnan Fan <pengnan.fan@mail.mcgill.ca>
2020-02-09 12:54:49 -0500

    reformat

--- DnsClient.py
+++ DnsClient.py
@@ -2,16 +2,22 @@ import scapy
 import socket
 import argparse
 
+
 def parseInput():
     parser = argparse.ArgumentParser("dns client")
-    parser.add_argument('-t', type=int, default=5, metavar='timeout', help='(optional) timeout gives how long to wait, in seconds, before retransmitting an unanswered query')
-    parser.add_argument('-r', type=int, default=3, metavar='max-retries', help='(optional) max-retries is the maximum number of times to retransmit an unanswered query before depriciation')
-    parser.add_argument('-p', type=int, default=53, metavar='port', help='(optional) port is the UDP port number of the DNS server')
+    parser.add_argument('-t', type=int, default=5, metavar='timeout',
+                        help='(optional) timeout gives how long to wait, in seconds, before retransmitting an unanswered query')
+    parser.add_argument('-r', type=int, default=3, metavar='max-retries',
+                        help='(optional) max-retries is the maximum number of times to retransmit an unanswered query before depriciation')
+    parser.add_argument('-p', type=int, default=53, metavar='port',
+                        help='(optional) port is the UDP port number of the DNS server')
     parser.add_argument('-mx', action='store_true', help='(optional) send a mail server query')
     parser.add_argument('-ns', action='store_true', help='(optional) send a name server query')
-    parser.add_argument('server', type=str, metavar='@server', help='IPv4 address of a server in the format of @a.b.c.d')
+    parser.add_argument('server', type=str, metavar='@server',
+                        help='IPv4 address of a server in the format of @a.b.c.d')
     parser.add_argument('name', type=str, metavar='name', help='domain name to query for')
     return parser.parse_args()
+
 
 def printHeader(config):
     print('DnsClient sending request for {}'.format(config.name))
@@ -28,6 +34,7 @@ def printHeader(config):
 def main():
     config = parseInput()
     printHeader(config)
+
 
 if __name__ == '__main__':
     main()
