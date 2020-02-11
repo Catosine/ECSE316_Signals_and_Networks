8bcca728f96097d6059eb07718ff925ce8c48875
Pengnan Fan <pengnan.fan@mail.mcgill.ca>
2020-02-11 16:00:22 -0500

    add timehout

--- DnsClient.py
+++ DnsClient.py
@@ -11,11 +11,13 @@ def parseInput():
                         help='(optional) max-retries is the maximum number of times to retransmit an unanswered query before termination')
     parser.add_argument('-p', type=int, default=53, metavar='port',
                         help='(optional) port is the UDP port number of the DNS server')
-    parser.add_argument('-mx', action='store_true', help='(optional) send a mail server query')
-    parser.add_argument('-ns', action='store_true', help='(optional) send a name server query')
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('-mx', default=False, action='store_true', help='(optional) send a mail server query')
+    group.add_argument('-ns', default=False, action='store_true', help='(optional) send a name server query')
     parser.add_argument('server', type=str, metavar='@server',
                         help='IPv4 address of a server in the format of @a.b.c.d')
     parser.add_argument('name', type=str, metavar='name', help='domain name to query for')
+    parser.print_help()
     return parser.parse_args()
 
 
@@ -34,12 +36,22 @@ def printHeader(config):
 def startClient(config):
     udp = socket(family=AF_INET, type=SOCK_DGRAM)
     udp.settimeout(config.t)
-    message = constructMsg(config.name.lower())
-    udp.sendto(parseMsg(message), (config.server[1:], config.p))
-    reply, _ = udp.recvfrom(4096)
+    message = constructMsg(config.name.lower(),config)
+    for i in range(config.r):
+        try:
+            udp.sendto(parseMsg(message), (config.server[1:], config.p))
+            reply, _ = udp.recvfrom(4096)
+        except:
+            print("Trial {} timeout".format(i))
+            if i >= config.r - 1:
+                print("ERROR\tMaximum number of retries {} exceeded".format(config.r))
+                exit(0)
+
     reply = parseReply(reply)
     ip = readIP(reply)
     print(ip)
+    print(reply)
+    decode_header(reply)
 
     udp.close()
 
@@ -67,8 +79,14 @@ def formatHex(hex):
     pairs = [" ".join(octets[i:i + 2]) for i in range(0, len(octets), 2)]
     return " ".join(pairs)
 
+def transInt(hex,digit):
+    res = 0
+    for i in range(digit):
+        res += hex_dex_lookup[hex[digit-1-i]]*(16**i)
+    return res
 
-def constructMsg(domain_name):
+
+def constructMsg(domain_name,config):
     output = "AA AA 01 00 00 01 00 00 00 00 00 00"
     domain_name = domain_name.split(".")
     for name in domain_name:
@@ -78,8 +96,49 @@ def constructMsg(domain_name):
         output += " " + size
         for char in name:
             output += " " + char_hex_lookup[char]
-    output += " 00 00 01 00 01"
+    output += " 00"
+    if   config.ns:
+        output += " 00 02 00 01"
+    elif config.mx:
+        output += " 00 0f 00 01"
+    else:
+        output += " 00 01 00 01"
     return output
+
+def decode_header(reply):
+    reply = reply.split()
+    id = reply[0]+reply[1]
+    print('id(hex) = {}'.format(id))
+    rcode = int(reply[3][1])
+    print('rcode(int) = {}'.format(rcode))
+    if rcode == 0:
+        print('No error condition')
+    else:
+        print('Sth is wrong')
+        return
+
+    qd_num = transInt(reply[4]+reply[5],4)
+    print('number of questions: {}'.format(qd_num))
+    an_num = transInt(reply[6]+reply[7],4)
+    print('number of answers: {}'.format(an_num))
+    ns_num = transInt(reply[8]+reply[9],4)
+    print('number of authorities: {}'.format(ns_num))
+    ar_num = transInt(reply[10]+reply[11],4)
+    print('number of additionals: {}'.format(ar_num))
+    ptr = 12
+    for i in range(qd_num):
+        ptr = readsection(reply,ptr)
+    for i in range(an_num):
+        ptr = readsection(reply,ptr)
+    for i in range(ns_num):
+        ptr = readsection(reply,ptr)
+    for i in range(ar_num):
+        ptr = readsection(reply,ptr)
+
+def readsection(reply,ptr):
+    return 0
+
+
 
 
 char_hex_lookup = {
